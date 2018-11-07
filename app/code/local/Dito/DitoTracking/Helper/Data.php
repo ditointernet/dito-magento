945cda3e82a5877a51126940d155292d90ad1d1d
Tannus Esquerdo <tannus.esquerdo@dito.com.br>
2018-11-07 14:26:26 -0200

    ajustes para envio de compra backend

--- app/code/local/Dito/DitoTracking/Helper/Data.php
+++ app/code/local/Dito/DitoTracking/Helper/Data.php
@@ -1,74 +1,89 @@
 <?php
-class Dito_DitoTracking_Helper_Data extends Mage_Core_Helper_Abstract {
-  public function getDitoEnabled($store = null){
-    return Mage::getStoreConfig('ditotracking_options/app_config/dito_enabled', $store);
+class Dito_DitoTracking_Helper_Data extends Mage_Core_Helper_Abstract 
+{
+
+  private function helper() {
+    $helper = Mage::helper('ditotracking/config');
+
+    return $helper;
   }
 
-  public function getApiKey($store = null){
-    return Mage::getStoreConfig('ditotracking_options/app_config/api_key', $store);
+  public function getUserId($customer) {
+    $id = '';
+
+    if($this->helper()->getIdType()) {
+      $cpf = $customer->getData($this->helper()->getUserDataConfig('user_config_cpf'));
+
+      if(!$cpf) {
+        return;
+      }
+      $id = $this->limpa_cpf($cpf);
+    } else {
+      $email = $customer->getEmail();
+      if(!$email) {
+        return;
+      }
+      $id = sha1($this->validate_email($customer->getEmail()));
+    }
+
+    return $id;
   }
 
-  public function getTrackStatus($key, $store = null){
-    return Mage::getStoreConfig('ditotracking_options/track_config/' . $key, $store);
+  private function validate_email($email) {
+    return preg_replace('/\s*/', '', strtolower($email));
   }
 
-  public function getUserDataConfig($key, $store = null){
-    return Mage::getStoreConfig('ditotracking_options/user_config/' . $key, $store);
+  private function limpa_cpf($valor) {
+    $valor = preg_replace('/[^0-9]/', '', $valor);
+    
+    return $valor;
   }
 
-  public function getCacheStrategy($store = null){
-    return Mage::getStoreConfig('ditotracking_options/cache_config/cache_strategy', $store);
-  }
+  public function getUserIdentifyObject($customer = null) {
+    $customer = ($customer) ? $customer : Mage::getSingleton('customer/session')->getCustomer();
 
-  public function getNewsEnabled($store = null){
-    return Mage::getStoreConfig('ditotracking_options/track_newsletter/dito_newsletter_enabled', $store);
-  }
-
-  public function getNewsDataConfig($key, $store = null){
-    return Mage::getStoreConfig('ditotracking_options/track_newsletter/' . $key, $store);
-  }
-
-  public function getUserIdentifyObject($customer){
-    $user = Array();
+    $user = array();
 
     if(isset($customer) && $customer->getId()) {
       $addresses = $customer->getAddresses();
       $city = '';
       $birthday = '';
-      $phoneFromAddress = '';
-      $phoneFromConfig = $customer->getData($this->getUserDataConfig('user_config_cellphone'));
+      $phoneFromConfig = $this->helper()->getUserDataConfig('user_config_cellphone');
       $phone = '';
+      $id = $this->getUserId($customer);
+      $cpfFromConfig = $this->helper()->getUserDataConfig('user_config_cpf');
 
       if(count($addresses) >= 1){
         $address = reset($addresses);
         if(isset($address)){
           $city = $address->getCity();
           $region = $address->getRegion();
-          $phoneFromAddress = $address->getTelephone();
         }
       }
-
-      $phone = isset($phoneFromConfig) ? $phoneFromConfig : $phoneFromAddress;
-      $email = preg_replace('/\s*/', '', strtolower($customer->getEmail()));
+      $cpf = empty($cpfFromConfig) ? '' : $customer->getData($cpfFromConfig);
+      $phone = empty($phoneFromConfig) ? '' : $customer->getData($phoneFromConfig);
+      $email = $this->validate_email($customer->getEmail());
 
       if($customer->getDob()){
         $birthday = explode(' ', $customer->getDob())[0];
       }
 
-      $user = Array(
-        'id' => sha1($email),
-        'email' => $email,
-        'name' => $customer->getName(),
-        'birthday' => $birthday,
-        'gender' => array('', 'male', 'female')[$customer->getGender()],
-        'location' => $city,
-        'data' => array(
-          'user_id' => $customer->getId(),
-          'cpf' => $customer->getData($this->getUserDataConfig('user_config_cpf')),
-          'telefone' => $phone,
-          'estado' => $region
-        )
-      );
+      if($id) {
+        $user = array(
+          'id' => $id,
+          'email' => $email,
+          'name' => $customer->getName(),
+          'birthday' => $birthday,
+          'gender' => array('', 'male', 'female')[$customer->getGender()],
+          'location' => $city,
+          'data' => array(
+            'user_id' => $customer->getId(),
+            'cpf' => $cpf,
+            'telefone' => $phone,
+            'estado' => $region
+          )
+        );
+      }
     }
 
     return $user;
@@ -79,10 +94,9 @@ class Dito_DitoTracking_Helper_Data extends Mage_Core_Helper_Abstract {
       return;
     }
 
-    $data = Array();
-
+    $data = array();
     $categoryIds = $product->getCategoryIds();
-    $categories = Array();
+    $categories = array();
 
     if(count($categoryIds)){
       foreach($categoryIds as $categoryId){
@@ -91,44 +105,22 @@ class Dito_DitoTracking_Helper_Data extends Mage_Core_Helper_Abstract {
       }
     }
 
-    if($product->getId()){
-      $data['id_produto'] = $product->getId();
-    }
-
-    if(count($categories)){
-      $data['categorias_produto'] = join(', ', $categories);
-    }
+    $data['id_produto'] = $product->getId();
+    $data['sku_produto'] = $product->getSku();
+    $data['nome_produto'] = $product->getName();
+    $data['categorias_produto'] = join(', ', $categories);
+    $data['tipo_produto'] = $product->getTypeId();
+    $data['preco_produto'] = floatval($product->getPrice());
+    $data['preco_final_produto'] = floatval($product->getFinalPrice());
+    $data['preco_especial_produto'] = floatval($product->getSpecialPrice());
+    $data['image'] = ($product) ? (string) Mage::helper('catalog/image')->init($product, 'image')->keepFrame(false)->resize(null, 438) : '';
 
     if($product->getStatus() == Mage_Catalog_Model_Product_Status::STATUS_ENABLED){
       $data['status_produto'] = 'Ativo';
     }
     else {
       $data['status_produto'] = 'Inativo';
-    }
-
-    if($product->getTypeId()){
-      $data['tipo_produto'] = $product->getTypeId();
-    }
-
-    if($product->getPrice()){
-      $data['preco_produto'] = floatval($product->getPrice());
-    }
-
-    if($product->getFinalPrice()){
-      $data['preco_final_produto'] = floatval($product->getFinalPrice());
-    }
-
-    if($product->getSpecialPrice()){
-      $data['preco_especial_produto'] = floatval($product->getSpecialPrice());
-    }
-
-    if($product->getName()){
-      $data['nome_produto'] = $product->getName();
-    }
-
-    if($product->getSku()){
-      $data['sku_produto'] = $product->getSku();
-    }
+    }    
 
     return $data;
   }
--- app/code/local/Dito/DitoTracking/Model/Observer.php
+++ app/code/local/Dito/DitoTracking/Model/Observer.php
@@ -1,6 +1,8 @@
 <?php
  
-class Dito_DitoTracking_Model_Observer {
+class Dito_DitoTracking_Model_Observer 
+{
+
   public function logCartAdd() {
     $helper = Mage::helper('ditotracking');
 
@@ -10,5 +12,187 @@ class Dito_DitoTracking_Model_Observer {
     $data['quantidade'] = floatval(Mage::app()->getRequest()->getParam('qty', 1));
  
     Mage::getModel('core/session')->setProductToShoppingCart(json_encode($data));
+  }
+
+  private function getOrderData($order) {
+    if(!$this->isEnabled()) {
+      return;
+    }
+
+    $orderdata = array();
+    $productsData = $this->getProductsFromOrder($order);
+    $customer_id = $order->getCustomerId();
+    $customer = Mage::getModel('customer/customer')->load($customer_id);
+    $customerData = $this->helper()->getUserIdentifyObject($customer);
+    $user_data = array();
+    $payment = $order->getPayment();
+
+    $orderdata['id_compra'] = $order->getIncrementId();
+    $orderdata['total'] = floatval($order->getGrandTotal());
+    $orderdata['subtotal'] = floatval($order->getSubtotal());
+    $orderdata['total_desconto'] = floatval($order->getDiscountAmount());
+    $orderdata['total_frete'] = floatval($order->getShippingAmount());
+    $orderdata['tipo_frete'] = floatval($order->getShippingMethod());
+    $orderdata['quantidade_produtos'] = floatval($order->getTotalQtyOrdered());
+    $orderdata['metodo_pagamento'] = $payment->getMethod();
+
+    $user_data['name'] = $customerData['name'];
+    $user_data['email'] = $customerData['email'];
+    $user_data['gender'] = $customerData['gender'];
+    $user_data['location'] = $customerData['location'];
+    $user_data['birthday'] = $customerData['birthday'];
+    $user_data['data'] = json_encode($customerData['data']);
+
+    return array($orderdata, $productsData, $user_data, $customer);
+  }
+
+  private function getProductsFromOrder($order) {
+    if(!$this->isEnabled()) {
+      return;
+    }
+
+    $productsData = array();
+    foreach ($order->getAllVisibleItems() as $item) {
+      $_Product = $item->getProduct();
+      $parent = $_Product->getParentItem();
+      if ($parent) {
+        $productsData[] = $this->helper()->getProductTrackingObject($parent);
+      } else {
+        $productsData[] = $this->helper()->getProductTrackingObject($_Product);
+      }
+    }
+
+    return $productsData;
+  }
+
+  private function sendOrderDataToServer($orderData, $customer) {
+    if (!$this->isEnabled()) {
+      return;
+    }
+
+    try {
+      $app_key = $this->config()->getApiKey();
+      $app_secret = $this->config()->getAppSecret();
+      $reference = $this->helper()->getUserId($customer);
+      $dito = new Dito_DitoTracking_Model_Utilities_DitoIns($app_key, $app_secret, $reference);
+
+      $dito->setCallbacks(function($obj) {
+        Mage::log("DITOPHPSDK ERROR - wrong request:" . json_encode($obj->getLastRequest()));
+      }, function() {
+
+      });
+      $message_id = sha1($this->config()->getApiKey() . $orderData['id_compra']);
+      $event = array(
+        'action' => 'comprou',
+        'revenue' => $orderData['total'],
+        'data' => json_encode($orderData)
+      );
+
+      $dito->prepare_event(
+        $message_id,
+        $event
+      )->send();
+    } catch(Exception $e) {
+      Mage::log("DITOPHPSDK ERROR:" . $e->getMessage() . ' more :' . json_encode($e));
+    }
+
+    return json_encode($dito->getLastRequest());
+  }
+
+  private function sendProductsDataToServer($orderData, $productsData, $customer) {
+    if (!$this->isEnabled()) {
+      return;
+    }
+
+    try {
+      $app_key = $this->config()->getApiKey();
+      $app_secret = $this->config()->getAppSecret();
+      $reference = $this->helper()->getUserId($customer);
+      $dito = new Dito_DitoTracking_Model_Utilities_DitoIns($app_key, $app_secret, $reference);
+
+      $dito->setCallbacks(function($obj) {
+        Mage::log("DITOPHPSDK ERROR - wrong request:" . json_encode($obj->getLastRequest()));
+      }, function() {
+
+      });
+      foreach($productsData as $product) {
+        $message_id = sha1($orderData['id_compra'] . $product['id_produto']);
+        $product['id_compra'] = $orderData['id_compra'];
+        $event = array(
+          'action' => 'comprou-produto',
+          'data' => json_encode($product)
+        );
+
+        $dito->prepare_event(
+          $message_id,
+          $event
+        )->send();
+      }
+      
+    } catch(Exception $e) {
+      Mage::log("DITOPHPSDK ERROR:" . $e->getMessage() . ' more :' . json_encode($e));
+    }
+
+    return json_encode($dito->getLastRequest());
+  }
+
+  private function sendIdentifyToServer($user_data, $customer) {
+    if (!$this->isEnabled()) {
+      return;
+    }
+
+    try {
+      $app_key = $this->config()->getApiKey();
+      $app_secret = $this->config()->getAppSecret();
+      $reference = $this->helper()->getUserId($customer);
+      $dito = new Dito_DitoTracking_Model_Utilities_DitoIns($app_key, $app_secret, $reference);
+
+      $dito->setCallbacks(function($obj) {
+        Mage::log("DITOPHPSDK ERROR - wrong request:" . json_encode($obj->getLastRequest()));
+      }, function() {
+
+      });
+
+      $dito->prepare_identify(
+        $user_data
+      )->send();
+      
+    } catch(Exception $e) {
+      Mage::log("DITOPHPSDK ERROR:" . $e->getMessage() . ' more :' . json_encode($e));
+    }
+
+    return json_encode($dito->getLastRequest());
+  }
+
+  public function export_new_order($observer) {
+    if (!$this->isEnabled()) {
+      return;
+    }
+
+    $order = $observer->getEvent()->getOrder();
+    $orderData = $this->getOrderData($order);
+    $this->sendIdentifyToServer($orderData[2], $orderData[3]);
+    $this->sendOrderDataToServer($orderData[0], $orderData[3]);
+    $this->sendProductsDataToServer($orderData[0], $orderData[1], $orderData[3]);
+
+    return $this;
+  }
+
+  private function isEnabled() {
+    $isEnabled = $this->config()->isDitoEnabled();
+
+    return $isEnabled;
+  }
+
+  private function config() {
+    $config = Mage::helper('ditotracking/config');
+
+    return $config;
+  }
+
+  private function helper() {
+    $helper = Mage::helper('ditotracking/data');
+
+    return $helper;
   }
 }
